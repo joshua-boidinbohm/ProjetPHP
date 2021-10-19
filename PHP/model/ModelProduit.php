e9f9dd85d7f3039d1346c63ed27e19aa316afd89
defla-x <defla-x@users.noreply.github.com>
2021-10-19 15:28:29 +0200

    init

--- PHP/model/ModelProduit.php
+++ PHP/model/ModelProduit.php
@@ -3,32 +3,32 @@
 require_once File::build_path(array("model","Model.php"));
 class ModelProduit{
 
-    private $modele;
-    private $nom;
-    private $puissance;
+    private $modelProduit;
+    private $nomProduit;
+    private $puissanceProduit;
 
     public function getModele() {
-        return $this->modele;
+        return $this->modelProduit;
     }
 
     public function getNom(){
-        return $this->nom;
+        return $this->nomProduit;
     }
 
     public function getPuissance(){
-        return $this->puissance;
+        return $this->puissanceProduit;
     }
 
     public function setModele($m) {
-        $this->modele = $m;
+        $this->modelProduit = $m;
     }
 
     public function setNom($n){
-        $this->nom = $n;
+        $this->nomProduit = $n;
     }
 
     public function setPuissance($p){
-        $this->puissance = $p;
+        $this->puissanceProduit = $p;
     }
 
     public function __construct($m = NULL, $n = NULL, $p = NULL) {
@@ -36,9 +36,9 @@ class ModelProduit{
             // Si aucun de $m et $p sont nuls,
             // c'est forcement qu'on les a fournis
             // donc on retombe sur le constructeur à 2 arguments
-            $this->modele = $m;
-            $this->nom = $n;
-            $this->puissance = $p;
+            $this->modelProduit = $m;
+            $this->nomProduit = $n;
+            $this->puissanceProduit = $p;
         }
     }
 
@@ -72,7 +72,7 @@ class ModelProduit{
             $req_prep->execute($values);
 
             // On récupère les résultats comme précédemment
-            $req_prep->setFetchMode(PDO::FETCH_CLASS, 'ModelVoiture');
+            $req_prep->setFetchMode(PDO::FETCH_CLASS, 'ModelProduit');
             $tab_prod = $req_prep->fetchAll();
             // Attention, si il n'y a pas de résultats, on renvoie false
             if (empty($tab_prod))
