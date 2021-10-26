f8227961e536d79c56d7647d4bd24e5fcf8e46b7
defla-x <defla-x@users.noreply.github.com>
2021-10-26 14:03:53 +0200

    user

--- PHP/index.php
+++ PHP/index.php
@@ -1,26 +1,6 @@
-<!DOCTYPE html>
-    <html>
-        <head>
-            <title>Bienvenue !</title>
-            <meta charset="utf-8">
-            <link rel="stylesheet" href="src/CSS/index.css">
-        </head>
-
-        <body >
-            <h1>Articles en vente</h1>
-
-            <p>
-                <?php
-                    $ROOT_FOLDER = __DIR__;
-                    $DS = DIRECTORY_SEPARATOR;
-                    require_once "{$ROOT_FOLDER}{$DS}lib{$DS}File.php";
-                    require_once File::build_path(array("controller","routeur.php"));
-                ?>
-            </p>
-        </body>
-    </html>
-
-
-
-
-
+<?php
+$ROOT_FOLDER = __DIR__;
+$DS = DIRECTORY_SEPARATOR;
+require_once "{$ROOT_FOLDER}{$DS}lib{$DS}File.php";
+require_once File::build_path(array("controller","routeur.php"));
+?>
