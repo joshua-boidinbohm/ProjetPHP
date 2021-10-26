d2bd188b94d14a9aae49e4b7a05515cd34656ede
boidinj <joshua.boidin-bohm@etu.umontpellier.fr>
2021-10-26 13:33:34 +0200

    css qui chemar

--- PHP/index.php
+++ PHP/index.php
@@ -1,9 +1,26 @@
-<?php
-$ROOT_FOLDER = __DIR__;
-$DS = DIRECTORY_SEPARATOR;
-require_once "{$ROOT_FOLDER}{$DS}lib{$DS}File.php";
-require_once File::build_path(array("controller","routeur.php"));
-echo '<p id="test">test</p>';
-?>
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title>Bienvenue !</title>
+            <meta charset="utf-8">
+            <link rel="stylesheet" href="CSS/index.css">
+        </head>
+
+        <body >
+            <h1>Articles en vente</h1>
+
+            <p>
+                <?php
+                    $ROOT_FOLDER = __DIR__;
+                    $DS = DIRECTORY_SEPARATOR;
+                    require_once "{$ROOT_FOLDER}{$DS}lib{$DS}File.php";
+                    require_once File::build_path(array("controller","routeur.php"));
+                ?>
+            </p>
+        </body>
+    </html>
+
+
+
 
 
