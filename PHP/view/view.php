26bae5a55b30dd20fe1316b60e38f82b189857b3
boidinj <joshua.boidin-bohm@etu.umontpellier.fr>
2021-11-30 13:18:33 +0100

    structure view.php + footer bien placé

--- PHP/view/view.php
+++ PHP/view/view.php
@@ -26,52 +26,54 @@
         </div>
     </header>
 </header>
-<main>
-    <body>
+<body>
+    <main>
         <?php
             $filepath = File::build_path(array("view", $controller, "$view.php"));
             require $filepath;
         ?>
-    </body>
-</main>
+    </main>
 
-<footer id="footer">
-    <link rel="stylesheet" href="src/CSS/style_footer.css">
+    <footer id="footer">
+        <link rel="stylesheet" href="src/CSS/style_footer.css">
 
-    <div class="footer" id="footer_child2">
-        <a class="link" href="https://theuselessweb.com/">Presse</a>
-        <a class="link" href="https://theuselessweb.com/">Contactez-nous</a>
-        <a class="link" href="https://theuselessweb.com">Politique de confidentialite</a>
-    </div>
-
-    <div class="footer" id="footer_child1">
-        <div class="texte_indication">
-            Inscrivez-vous à notre newsletter pour être au courrant de toute l'actualité
+        <div class="footer" id="footer_child2">
+            <a class="link" href="https://theuselessweb.com/">Presse</a>
+            <a class="link" href="https://theuselessweb.com/">Contactez-nous</a>
+            <a class="link" href="https://theuselessweb.com">Politique de confidentialite</a>
         </div>
-        <div>
-            <form method="get">
-            <fieldset id="fieldset">
-                <input pattern="[a-z0-9._-]+@[a-z0-9.-]+\.[a-z]{2,4}" type="email" placeholder="yourmail@domain.com" id="email" name="email" required>
-                <input id="valider" type="submit">
-                <input type='hidden' name='action' value='newslettered'>
-            </fieldset>
-        </div>
-    </div>
 
-    <div  class="footer" id="footer_child3">
-        <div class="texte_indication"> Suivez nous sur les réseaux :</div>
-        <div id="logo_container">
-            <div class="logo">
-                <a href="https://facebook.com" target="_blank"><img src="src/images/Webp.net-resizeimage.png" alt="facebook" title="Y aller"></a>
+        <div class="footer" id="footer_child1">
+            <div class="texte_indication">
+                Inscrivez-vous à notre newsletter pour être au courrant de toute l'actualité
             </div>
-            <div class="logo">
-                <a href="https://www.instagram.com/" target="_blank"><img src="src/images/Webp.net-resizeimage(2).png" alt="instagram" title="Y aller" ></a>
-            </div>
-            <div class="logo">
-                <a href="https://twitter.com" target="_blank"><img src="src/images/Webp.net-resizeimage(3).png" alt="twitter" title="Y aller" ></a>
-
+            <div>
+                <form method="get">
+                    <fieldset id="fieldset">
+                        <input pattern="[a-z0-9._-]+@[a-z0-9.-]+\.[a-z]{2,4}" type="email" placeholder="yourmail@domain.com" id="email" name="email" required>
+                        <input id="valider" type="submit">
+                        <input type='hidden' name='action' value='newslettered'>
+                    </fieldset>
             </div>
         </div>
-    </div>
-</footer>
+
+        <div  class="footer" id="footer_child3">
+            <div class="texte_indication"> Suivez nous sur les réseaux :</div>
+            <div id="logo_container">
+                <div class="logo">
+                    <a href="https://facebook.com" target="_blank"><img src="src/images/Webp.net-resizeimage.png" alt="facebook" title="Y aller"></a>
+                </div>
+                <div class="logo">
+                    <a href="https://www.instagram.com/" target="_blank"><img src="src/images/Webp.net-resizeimage(2).png" alt="instagram" title="Y aller" ></a>
+                </div>
+                <div class="logo">
+                    <a href="https://twitter.com" target="_blank"><img src="src/images/Webp.net-resizeimage(3).png" alt="twitter" title="Y aller" ></a>
+
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+
+
 </html>
