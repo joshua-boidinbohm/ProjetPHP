b550341316a19999c468027a3f10549cd4405c27
friast <thomas-frias@etu.umontpellier.fr>
2021-11-30 14:07:56 +0100

    admin page avec liste des produit en BD

--- PHP/view/utilisateur/admin.php
+++ PHP/view/utilisateur/admin.php
@@ -1,3 +1,22 @@
+<h1>
+    Onglet Administration
+</h1>
+<h2>
+    Liste des produits :
+</h2>
 <?php
-echo 'bonjour';
+$tab_v = ModelProduit::getAllProduits();
+foreach ($tab_v as $v) {
+    $modhtml = htmlspecialchars($v->getModele());
+    $modurl = rawurlencode($v->getModele());
+    echo '<p id="article"> 
+                    Produit de modèle ' . $modhtml . ' <a href="?action=delete&mod=' . $modurl . '">(- supprimer)</a>
+              </p>';
+}
 ?>
+<h2>
+    Ajouter un produit :
+</h2>
+<p>
+    à implémenter ! il faut que l'on puisse ajouter une voiture en BD.
+</p>
