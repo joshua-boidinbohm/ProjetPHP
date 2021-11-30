9ea60a3568936dd046e6e2656e98755ecdd900b1
boidinj <joshua.boidin-bohm@etu.umontpellier.fr>
2021-11-30 13:54:49 +0100

    le stayiyle

--- PHP/view/produit/list.php
+++ PHP/view/produit/list.php
@@ -3,11 +3,13 @@ foreach ($tab_v as $v){
     $modhtml = htmlspecialchars($v->getModele());
     $modurl = rawurlencode($v->getModele());
 
-    echo '<div class="img"> <img src="src/images/panneau1.jpeg">
-           </div>';
 
 
-    echo '<p id="article"> Produit de modèle '. $modhtml . ' <a href="?action=read&mod=' . $modurl . '">(+ d\'info)</a></p>';
+
+    echo '<p id="article"> 
+                <img src="src/images/'. $modhtml .'.jpeg"> '.' <br>
+                Produit de modèle '. $modhtml . ' <a href="?action=read&mod=' . $modurl . '">(+ d\'info)</a>
+          </p>';
 
 
 }
